2eecb62f91a3ecdba5b742d897f4db9c288f38c8
wilo0510 <wrpachonl@gmail.com>
2018-05-15 15:12:44 -0500

    Update lexer_prueba.py

--- lexer_prueba.py
+++ lexer_prueba.py
@@ -1,1 +1,11 @@
-
+import lexer_rules
+from ply.lex import lex
+archivo = open("datos.txt")
+text = archivo.read()
+archivo.close()
+lexer = lex(module=lexer_rules)
+lexer.input(text)
+token = lexer.token()
+while token is not None:
+    print (token.type, token.value)
+    token = lexer.token()
